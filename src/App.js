5cf2d6e723cd1c1b8c410a838ec76bef9e067423
AggressiveFish <roberthpike@yahoo.com>
2022-01-03 20:41:42 -0500

    Another commit

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <div className="text-primary">Hello World, from {userName}</div>
+          <div className="text-primary">Hello {userName}</div>
       </header>
     </div>
   );
